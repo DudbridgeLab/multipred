a26e63651bed191ccd82c7717f79b41c547c356c
Frank Dudbridge <frank.dudbridge@leicester.ac.uk>
2020-07-10 15:02:13 +0100

    Add files via upload

--- R/multipred.R
+++ R/multipred.R
@@ -2,13 +2,13 @@
 #'
 #' Package for calculating measures of accuracy for risk predictors of multiple outcomes.
 #'
-#' Accuracy can be evaluated in four senses: outcome-wise, joint, and family-wise (weak sense and strong sense).
-#' For convenience the weak family-wise sense is also called "screening", and the strong family-wise sense
-#' simply "family-wise".  In each sense, accuracy can be measured empirically, within data sets given as input, or theoretically,
+#' Accuracy can be evaluated in four senses: outcome-wise, joint, and panel-wise (weak sense and strong sense).
+#' For convenience the weak panel-wise sense is also called "screening", and the strong panel-wise sense
+#' simply "panel-wise".  In each sense, accuracy can be measured empirically, within data sets given as input, or theoretically,
 #' given parameters of an underlying multivariate liability threshold model.
 #'
 #' Throughout the documentation, an "outcome" means one of several binary variables observed in an individual,
-#' and that an outcome "occurs" when the variable has the positive state.
+#' and an outcome "occurs" when the variable has the positive state.
 #'
 #' Outcome-wise measures calculate standard univariate measures of accuracy over all outcomes and individuals.
 #'
@@ -17,7 +17,7 @@
 #' Screening measures consider the prediction of at least one outcome occuring within an individual.
 #' It is not necessary that the predicted outcomes are the same ones that actually occur.
 #'
-#' Family-wise measures consider the prediction of at least one outcome occuring within an individual.
+#' Panel-wise measures consider the prediction of at least one outcome occuring within an individual.
 #' There must be at least one predicted outcome that actually occurs.
 #'
 #' @section Functions:
@@ -27,7 +27,7 @@
 #'
 #' \code{\link{screening}}
 #'
-#' \code{\link{familyWise}}
+#' \code{\link{panelWise}}
 #'
 #' \code{\link{analyticOutcomeWise}}
 #'
@@ -35,7 +35,7 @@
 #'
 #' \code{\link{analyticScreening}}
 #'
-#' \code{\link{analyticFamilyWise}}
+#' \code{\link{analyticPanelWise}}
 #'
 #' @docType package
 #' @name multipred
